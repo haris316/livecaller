e31aa86db80a0931f0ad74764569463da126c16d
haris316 <gogeto931@gmail.com>
2025-08-10 01:34:42 +0500

    back to normal

--- routes/live/route.js
+++ routes/live/route.js
@@ -16,121 +16,121 @@ router.get('/', async (req, res) => {
     //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     //Updating Scores
     console.log("Updating Scores");
-    // try {
-    //     // let db = await connectToDb();
-    //     const api_url = "https://api.sportmonks.com/v3/football/livescores?include=round;stage;league;venue;state;lineups.details.type;events;timeline;statistics;periods;participants;scores;"
-    //     const agent = new https.Agent({
-    //         rejectUnauthorized: false,
-    //     });
-    //     axios.defaults.httpsAgent = agent
-    //     let full_URL = api_url
-    //     let match_data = [];
-    //     let response = await axios.get(full_URL, {
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Accept": "application/json",
-    //             "Authorization": process.env.NEXT_PUBLIC_SPORTMONKS_TOKEN
-    //         },
-    //         agent: agent
-    //     })
-    //     if (response.status === 200) {
-    //         if (response.data && response.data.data) {
-    //             match_data = response.data;
-    //             const match_array = match_data.data;
+    try {
+        // let db = await connectToDb();
+        const api_url = "https://api.sportmonks.com/v3/football/livescores?include=round;stage;league;venue;state;lineups.details.type;events;timeline;statistics;periods;participants;scores;"
+        const agent = new https.Agent({
+            rejectUnauthorized: false,
+        });
+        axios.defaults.httpsAgent = agent
+        let full_URL = api_url
+        let match_data = [];
+        let response = await axios.get(full_URL, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": process.env.NEXT_PUBLIC_SPORTMONKS_TOKEN
+            },
+            agent: agent
+        })
+        if (response.status === 200) {
+            if (response.data && response.data.data) {
+                match_data = response.data;
+                const match_array = match_data.data;
 
-    //             for (const match of match_array) {
-    //                 // LINE-UPS
-    //                 const lineups = match.lineups.map(lineup => ({
-    //                     player_id: lineup.player_id,
-    //                     player_name: lineup.player_name,
-    //                     team_id: lineup.team_id,
-    //                     team_name: match.participants.find(x => x.id === lineup.team_id).name,
-    //                     position_id: lineup.position_id,
-    //                     position_name: event_type_ids[lineup.position_id] || "Unknown",
-    //                     formation_position: lineup.formation_position,
-    //                     jersey_number: lineup.jersey_number,
-    //                     statistics: lineup.details.map((stat) => {
-    //                         return {
-    //                             value: Number(stat?.data?.value) || null,
-    //                             name: stat?.type?.name || null,
-    //                             code: stat?.type?.code || null
-    //                         }
-    //                     })
-    //                 }));
+                for (const match of match_array) {
+                    // LINE-UPS
+                    const lineups = match.lineups.map(lineup => ({
+                        player_id: lineup.player_id,
+                        player_name: lineup.player_name,
+                        team_id: lineup.team_id,
+                        team_name: match.participants.find(x => x.id === lineup.team_id).name,
+                        position_id: lineup.position_id,
+                        position_name: event_type_ids[lineup.position_id] || "Unknown",
+                        formation_position: lineup.formation_position,
+                        jersey_number: lineup.jersey_number,
+                        statistics: lineup.details.map((stat) => {
+                            return {
+                                value: Number(stat?.data?.value) || null,
+                                name: stat?.type?.name || null,
+                                code: stat?.type?.code || null
+                            }
+                        })
+                    }));
 
-    //                 // EVENTS
-    //                 const events = match.events.map(event => ({
-    //                     team_id: event.participant_id,
-    //                     team_name: match.participants.find(x => x.id === event.participant_id).name,
-    //                     event_id: event.type_id,
-    //                     event_name: event_type_ids[event.type_id] || "Unknown",
-    //                     player_id: event.player_id,
-    //                     player_name: event.player_name,
-    //                     related_player_id: event.related_player_id,
-    //                     related_player_name: event.related_player_name,
-    //                     result: event.result,
-    //                     info: event.info,
-    //                     addition: event.addition,
-    //                     minute: event.minute,
-    //                     sort_order: event.sort_order,
-    //                 }));
+                    // EVENTS
+                    const events = match.events.map(event => ({
+                        team_id: event.participant_id,
+                        team_name: match.participants.find(x => x.id === event.participant_id).name,
+                        event_id: event.type_id,
+                        event_name: event_type_ids[event.type_id] || "Unknown",
+                        player_id: event.player_id,
+                        player_name: event.player_name,
+                        related_player_id: event.related_player_id,
+                        related_player_name: event.related_player_name,
+                        result: event.result,
+                        info: event.info,
+                        addition: event.addition,
+                        minute: event.minute,
+                        sort_order: event.sort_order,
+                    }));
 
-    //                 // TEAMS
-    //                 const teams = match.participants.map(team => {
-    //                     let team_short_code = team.short_code;
-    //                     if (!team_short_code) {
-    //                         team_short_code = team.name.slice(0, 3).toUpperCase();
-    //                     }
+                    // TEAMS
+                    const teams = match.participants.map(team => {
+                        let team_short_code = team.short_code;
+                        if (!team_short_code) {
+                            team_short_code = team.name.slice(0, 3).toUpperCase();
+                        }
 
-    //                     return {
-    //                         team_id: team.id,
-    //                         team_name: team.name,
-    //                         short_code: team_short_code,
-    //                         image_path: team.image_path,
-    //                         location: team.meta.location,
-    //                         winner: team.meta.winner,
-    //                     };
-    //                 });
+                        return {
+                            team_id: team.id,
+                            team_name: team.name,
+                            short_code: team_short_code,
+                            image_path: team.image_path,
+                            location: team.meta.location,
+                            winner: team.meta.winner,
+                        };
+                    });
 
-    //                 // SCORES
-    //                 const scores = match.scores.map(score => ({
-    //                     score_type_id: score.type_id,
-    //                     score_type_name: event_type_ids[score.type_id] || "Unknown",
-    //                     team_id: score.participant_id,
-    //                     team_name: match.participants.find(x => x.id === score.participant_id).name,
-    //                     team_type: score.score.participant,
-    //                     goals: score.score.goals,
-    //                 }));
+                    // SCORES
+                    const scores = match.scores.map(score => ({
+                        score_type_id: score.type_id,
+                        score_type_name: event_type_ids[score.type_id] || "Unknown",
+                        team_id: score.participant_id,
+                        team_name: match.participants.find(x => x.id === score.participant_id).name,
+                        team_type: score.score.participant,
+                        goals: score.score.goals,
+                    }));
 
-    //                 // Consolidating data into QUERY Object
-    //                 const query = {
-    //                     id: match.id,
-    //                     seasonID: Number(process.env.NEXT_PUBLIC_SEASON_ID),
-    //                     gameweekID: match.round.id,
-    //                     gameweekName: match.round.name,
-    //                     name: match.name,
-    //                     starting_at: match.starting_at,
-    //                     result_info: match.result_info,
-    //                     state: match.state.name,
-    //                     lineups: lineups,
-    //                     events: events,
-    //                     teams: teams,
-    //                     scores: scores,
-    //                 };
-    //                 // db = await connectToDb();
-    //                 const res = await Match.updateOne({ id: match.id }, { $set: query }, { upsert: true });
-    //             }
-    //             console.log("done")
-    //         } else {
-    //             console.log("no match being played")
-    //         }
-    //     } else {
-    //         console.log("Failed to fetch data from API");
-    //     }
-    // } catch (err) {
-    //     console.log(err);
-    //     console.log("An unexpected error occurred while updating the scores. Please try again.");
-    // }
+                    // Consolidating data into QUERY Object
+                    const query = {
+                        id: match.id,
+                        seasonID: Number(process.env.NEXT_PUBLIC_SEASON_ID),
+                        gameweekID: match.round.id,
+                        gameweekName: match.round.name,
+                        name: match.name,
+                        starting_at: match.starting_at,
+                        result_info: match.result_info,
+                        state: match.state.name,
+                        lineups: lineups,
+                        events: events,
+                        teams: teams,
+                        scores: scores,
+                    };
+                    // db = await connectToDb();
+                    const res = await Match.updateOne({ id: match.id }, { $set: query }, { upsert: true });
+                }
+                console.log("done")
+            } else {
+                console.log("no match being played")
+            }
+        } else {
+            console.log("Failed to fetch data from API");
+        }
+    } catch (err) {
+        console.log(err);
+        console.log("An unexpected error occurred while updating the scores. Please try again.");
+    }
 
     //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     //Updating Standings
