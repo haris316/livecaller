d01f21f92b93970af1b670e1230c5ea9c884a0ed
haris316 <gogeto931@gmail.com>
2025-08-20 03:22:15 +0500

    Weekly Ratings

--- routes/weekly/route.js
+++ routes/weekly/route.js
@@ -1,123 +1,79 @@
 import express from 'express';
-import { Match, Player } from '../../models.js';
+import axios from 'axios';
+import https from 'https';
+import { Player, FantasyLeague, FantasyTeam, Team, GameWeek, Match, Standing } from '../../models.js';
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    // Getting current season ID
-    const seasonID = process.env.NEXT_PUBLIC_SEASON_ID
-
-    //Getting Matches update recently
-    let minutes_since_last_update = new Date(Date.now() - 1000 * 60 * 60 * 30);
-
-    let matchesUpdated = await Match.find({
-        updatedAt: { $gte: minutes_since_last_update }
-    })
-    if (matchesUpdated && matchesUpdated.length > 0) {
-        let players_updated = matchesUpdated.map((match) => {
-            let match_gameweek_id = match.gameweekID;
-            return match.lineups.map((lineup) => {
-                let lineup_obj = {}
-                lineup_obj.gameweekID = match_gameweek_id;
-                lineup_obj.item = lineup;
-                return lineup_obj;
-            })
-        }).flat()
-
-        let points_config = [
-            "goals",
-            "assists",
-            "clean-sheet",
-            "goals-conceded",
-            "penalty_save",
-            "saves",
-            "penalty_miss",
-            "yellowcards",
-            "redcards",
-            "minutes-played",
-            "tackles",
-            "interceptions"
-        ]
-        let players_fpl = []
-        players_updated.forEach((player) => {
-            let player_stats = player.item.statistics;
-            let fpl_stats = {
-                "goals": 0,
-                "assists": 0,
-                "clean-sheet": 0,
-                "goals-conceded": 0,
-                "penalty_save": 0,
-                "saves": 0,
-                "penalty_miss": 0,
-                "yellowcards": 0,
-                "redcards": 0,
-                "minutes-played": 0,
-                "tackles": 0,
-                "interceptions": 0,
-                "bonus": 0
-            };
-            if (player_stats && player_stats.length > 0) {
-                player_stats.forEach((stat) => {
-                    if (points_config.indexOf(stat.code) !== -1) {
-                        fpl_stats[stat.code] = stat.value;
-                    }
-                })
+router.get('/players', async (req, res) => {
+    try {
+        let bulkOps = [];
+        const seasonID = process.env.NEXT_PUBLIC_SEASON_ID
+        const api_url = "https://api.sportmonks.com/v3/football/players/"
+        const url_options = "?include=nationality;position;detailedPosition;teams;statistics.details;"
+        const agent = new https.Agent({
+            rejectUnauthorized: false,
+        });
+        axios.defaults.httpsAgent = agent
+        let playerIDs = await Player.find({})
+        playerIDs = playerIDs.map((player) => {
+            return {
+                playerID: player.id,
+                teamID: player.teamID,
             }
-            if ((fpl_stats["goals-conceded"] === 0) && (fpl_stats["minutes-played"] > 60)) fpl_stats["clean-sheet"] = 1;
-            // console.log(player.item.player_name);
-            // console.log(fpl_stats);
-            // console.log("XXXXXXXXXXXX");
-            player.item.fpl_stats = fpl_stats;
-            players_fpl.push({
-                _id: player.item._id,
-                player_id: player.item.player_id,
-                name: player.item.player_name,
-                gameweekID: player.gameweekID,
-                fpl_stats: fpl_stats
-            })
         })
+        for (const player of playerIDs) {
+            const playerID = player.playerID;
+            const full_URL = api_url + playerID + url_options;
+            console.log("full_URL : " + full_URL)
+            const response = await axios.get(full_URL, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": process.env.NEXT_PUBLIC_SPORTMONKS_TOKEN
+                },
+                agent: agent
+            });
 
-
-        // let currentGameWeek = await GameWeek.findOne({ is_current: true })
-        let allPlayers = await Player.find({}).populate("points.gameweek")
-
-
-        allPlayers.forEach(async (player) => {
-            // console.log(player.name);
-            let player_fpl = players_fpl.find((player_fpl) => player_fpl.player_id == player.id)
-            if (player_fpl) {
-                let playerGameWeek = player.points.find((item) => ((item.gameweek.id == player_fpl.gameweekID) && (item.seasonID == seasonID)))
-                if (playerGameWeek) {
-                    playerGameWeek.fpl_stats = player_fpl.fpl_stats;
-                    let temp_points = 0;
-                    if (player_fpl.fpl_stats["minutes-played"] > 60) temp_points = temp_points + 2
-                    else if (player_fpl.fpl_stats["minutes-played"] > 30) temp_points = temp_points + 1
-                    temp_points = temp_points + (player_fpl.fpl_stats.goals * 5);
-                    temp_points = temp_points + (player_fpl.fpl_stats.assists * 3);
-                    if (player.position_name == "Goalkeeper" || player.position_name == "Defender") temp_points = temp_points + (player_fpl.fpl_stats["clean-sheet"] * 4);
-                    if (player.position_name == "Goalkeeper" || player.position_name == "Defender") temp_points = temp_points + (player_fpl.fpl_stats["goals-conceded"] * -1);
-                    temp_points = temp_points + (player_fpl.fpl_stats.penalty_save * 5);
-                    temp_points = temp_points + Math.floor(player_fpl.fpl_stats.saves * 0.333);
-                    temp_points = temp_points + (player_fpl.fpl_stats.penalty_miss * -3);
-                    temp_points = temp_points + (player_fpl.fpl_stats.yellowcards * -1);
-                    temp_points = temp_points + (player_fpl.fpl_stats.redcards * -3);
-                    temp_points = temp_points + Math.floor(player_fpl.fpl_stats.tackles * 0.2);
-                    temp_points = temp_points + (player_fpl.fpl_stats.interceptions * 0.2);
-                    temp_points = temp_points + (player_fpl.fpl_stats.bonus);
-                    if (temp_points > 0) temp_points = Math.floor(temp_points)
-                    else temp_points = Math.ceil(temp_points);
-                    playerGameWeek.points = temp_points;
+            if (response.status !== 200) {
+                console.log(`Failed to fetch data for player ${playerID}`);
+                continue;
+            }
+            try {
+                const player_data = response.data.data;
+                const rating = player_data?.statistics?.filter(i => i.season_id == seasonID)[0]?.details?.filter(i => i.type_id == 118)[0]?.value?.average || 0;
+                if (rating != 0) {
+                    console.log(`Updating rating for ${player_data.name}: ${rating}`);
+                    bulkOps.push({
+                        updateOne: {
+                            filter: { id: player.playerID },
+                            update: { $set: { rating: rating } }
+                        }
+                    });
                 }
-                let res = await player.save();
+            } catch {
+                continue
             }
-        })
+        }
+
+        // Execute batch update
+        if (bulkOps.length > 0) {
+            await Player.bulkWrite(bulkOps);
+            console.log(`Updated ratings for ${bulkOps.length} players.`);
+        }
+
+        return res.status(200).json({
+            error: false,
+            message: `Updated ratings for ${bulkOps.length} players.`
+        });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(200).json({
+            error: true,
+            message: "An unexpected error occurred. Please try again later.",
+        });
     }
-    // }
-    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    //End of Call
-    return res.status(200).json({
-        error: "maybe",
-        message: "done"
-    });
+
 });
 
 export default router;
