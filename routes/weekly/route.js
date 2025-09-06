ac2f7449d86b725bc7a1358b3bf06079391dc901
haris316 <gogeto931@gmail.com>
2025-09-06 05:52:27 +0500

    Unstoppable Ach

--- routes/weekly/route.js
+++ routes/weekly/route.js
@@ -1,8 +1,86 @@
 import express from 'express';
 import axios from 'axios';
 import https from 'https';
-import { Player, FantasyLeague, FantasyTeam, Team, GameWeek, Match, Standing } from '../../models.js';
+import { Player, FantasyLeague, FantasyTeam, Team, GameWeek, Match, Standing, User, FantasyAchievement } from '../../models.js';
+import mongoose from 'mongoose';
 const router = express.Router();
+
+router.get('/unstoppable', async (req, res) => {
+    try {
+        // Parameter to set number of wins required
+        const win_number = 8;
+        let ach = await FantasyAchievement.findOne({ name: "UNSTOPPABLE" });
+        // Fetch all users and parse one by one
+        let users = await User.find({}).populate("achievements.achievement");
+        for (const user of users) {
+            console.log("Processing user: " + user.email);
+            let ach_count = 0;
+            // Fetch all leagues where user is present and format is H2H
+            let leagues = await FantasyLeague.find({ users_onboard: user.email, is_deleted: false, 'league_configuration.format': 'Head to Head' }).populate("head_to_head_points.team")
+            for (const league of leagues) {
+                console.log(" Processing league: " + league.league_name);
+                // Fetch user's team in the league
+                for (const team of league.head_to_head_points) {
+                    // console.log("user email : "+user.email)
+                    // console.log(team.team.user_email);
+                    // console.log("team email : "+team.user_email)
+                    if (team.team.user_email === user.email) {
+                        // Convert form to array and check the form of the team for number of consecutive wins
+                        let form_array = team.form.split(" ");
+                        console.log(form_array);
+                        let win_count = 0;
+                        for (const form of form_array) {
+                            // If win, increase win_count
+                            if (form === "W") win_count++;
+                            // else reset win_count
+                            else win_count = 0
+                            // If win_count reaches win_number, increase ach_count and reset win_count
+                            if (win_count === win_number) {
+                                ach_count++;
+                                win_count = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            // When all leagues of user are processed. Check if ach_count > 0, update user's achievements
+            if (ach_count > 0) {
+                let ach_found = false
+                for (const ach_obj of user.achievements) {
+                    // console.log(ach_obj)
+                    // console.log(ach)
+                    if (ach_obj.achievement.name === ach.name) {
+                        console.log("found")
+                        ach_found = true;
+                        ach_obj.unlocked = true;
+                        ach_obj.count = ach_count;
+                    }
+                }
+                if (!ach_found) {
+                    console.log("still not found")
+                    user.achievements.push({
+                        achievement: new mongoose.Types.ObjectId(ach._id),
+                        unlocked: true,
+                        count: ach_count
+                    })
+                }
+            }
+            // Save user
+            await user.save();
+        }
+        return res.status(200).json({
+            error: false,
+            message: "UNSTOPPABLE - Processed Successfully.",
+        });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(200).json({
+            error: true,
+            message: "An unexpected error occurred. Please try again later.",
+        });
+    }
+})
 
 router.get('/players', async (req, res) => {
     try {
